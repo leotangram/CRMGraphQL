8eeab538b7c46b71388e353e6543d109196e916d
Leonardo Omaña <leotangram@gmail.com>
2021-01-12 16:56:15 -0500

    Add a second resolver

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const { ApolloServer, gql } = require('apollo-server')
 const typeDefs = gql`
   type Course {
     title: String
+  }
+
+  type Technology {
     technology: String
   }
 
   type Query {
     getCourses: [Course]
+    getTechnology: [Technology]
   }
 `
 
@@ -34,7 +38,8 @@ const courses = [
 // Resolvers
 const resolvers = {
   Query: {
-    getCourses: () => courses
+    getCourses: () => courses,
+    getTechnology: () => courses
   }
 }
 
