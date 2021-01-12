ad6071b4603f7f0400c13ae8ce51ed4eddb073a7
Leonardo Omaña <leotangram@gmail.com>
2021-01-12 16:53:34 -0500

    Add Types and Resolvers

--- index.js
+++ index.js
@@ -1,6 +1,47 @@
 const { ApolloServer, gql } = require('apollo-server')
 
-const server = new ApolloServer()
+// Schema
+const typeDefs = gql`
+  type Course {
+    title: String
+    technology: String
+  }
+
+  type Query {
+    getCourses: [Course]
+  }
+`
+
+const courses = [
+  {
+    title: 'JavaScript Moderno Guía Definitiva Construye +10 Proyectos',
+    technology: 'JavaScript ES6'
+  },
+  {
+    title: 'React – La Guía Completa: Hooks Context Redux MERN +15 Apps',
+    technology: 'React'
+  },
+  {
+    title: 'Node.js – Bootcamp Desarrollo Web inc. MVC y REST API’s',
+    technology: 'Node.js'
+  },
+  {
+    title: 'ReactJS Avanzado – FullStack React GraphQL y Apollo',
+    technology: 'React'
+  }
+]
+
+// Resolvers
+const resolvers = {
+  Query: {
+    getCourses: () => courses
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
 
 server.listen().then(({ url }) => {
   console.log(`Server ready in URL: ${url}`)
