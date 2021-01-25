4fe7cdb1421417d35ccaeb8947424c8927f9807d
Leonardo Omaña <leotangram@gmail.com>
2021-01-25 10:25:27 -0500

    El context en GraphQL

--- src/db/resolvers.js
+++ src/db/resolvers.js
@@ -1,31 +1,5 @@
-const courses = [
-  {
-    title: 'JavaScript Moderno Guía Definitiva Construye +10 Proyectos',
-    technology: 'JavaScript ES6'
-  },
-  {
-    title: 'React – La Guía Completa: Hooks Context Redux MERN +15 Apps',
-    technology: 'React'
-  },
-  {
-    title: 'Node.js – Bootcamp Desarrollo Web inc. MVC y REST API’s',
-    technology: 'Node.js'
-  },
-  {
-    title: 'ReactJS Avanzado – FullStack React GraphQL y Apollo',
-    technology: 'React'
-  }
-]
-
 const resolvers = {
-  Query: {
-    getCourses: (_, { input, ctx }) => {
-      const result = courses.filter(
-        ({ technology }) => technology === input.technology
-      )
-      return result
-    }
-  }
+  Query: {}
 }
 
 module.exports = resolvers
