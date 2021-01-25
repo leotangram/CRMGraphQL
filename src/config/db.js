f3ce9b235488f56e25a43d2c1dec23a5157a3f23
Leonardo Omaña <leotangram@gmail.com>
2021-01-25 11:16:40 -0500

    Connect db with mongoose

---
+++ src/config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+require('dotenv').config({ path: '.env' })
+
+const dbConnect = async () => {
+  try {
+    await mongoose.connect(process.env.DB_MONGO, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true
+    })
+    console.log('DB connected')
+  } catch (error) {
+    console.log('Hubo un error')
+    console.log(error)
+    process.exit(1) // Stop the App
+  }
+}
+
+module.exports = dbConnect
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const { ApolloServer } = require('apollo-server')
 const typeDefs = require('./db/schema')
 const resolvers = require('./db/resolvers')
+const dbConnect = require('./config/db')
+
+dbConnect()
 
 const server = new ApolloServer({
   typeDefs,
